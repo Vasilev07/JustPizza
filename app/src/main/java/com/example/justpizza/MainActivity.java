fa195d9cfdfe6eeb8b55931e4363367932bfc1b6
Vasilev07 <georgevasilev07@gmail.com>
2020-12-04 18:04:05 +0200

    added some new features

--- app/src/main/java/com/example/justpizza/MainActivity.java
+++ app/src/main/java/com/example/justpizza/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.justpizza;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
     TextView quantityNumberView;
+    TextView priceTextView;
     int quantity = 1;
+    int pizzaPrice = 4;
+    private RadioGroup pizzaType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +25,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         quantityNumberView = findViewById(R.id.quantityNumber);
-//        Button button = findViewById(R.id.order);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, display(1).toString(), Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        priceTextView = findViewById(R.id.price_text_view);
+        pizzaType = (RadioGroup)findViewById(R.id.pizza_type);
+        pizzaType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                View additionsGroup = findViewById(R.id.additions);
+
+                RadioButton radioButton = (RadioButton) findViewById(checkedId);
+
+                Toast.makeText(getBaseContext(), radioButton.getText(), Toast.LENGTH_SHORT).show();
+
+                if (radioButton.getText().equals("Just Pizza")) {
+                    additionsGroup.setVisibility(View.VISIBLE);
+                } else {
+                    additionsGroup.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     public void submitOrder(View view) {
@@ -35,12 +54,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
+        if (quantity == 100) {
+            Toast.makeText(this, "Can order more than 100 pizzas", Toast.LENGTH_SHORT).show();
+
+            return;
+        }
+
         quantity += 1;
         display(quantity);
     }
 
     public void decrement(View view) {
+        if (quantity == 1) {
+            Toast.makeText(this, "Can order less than 1 pizza", Toast.LENGTH_SHORT).show();
+
+            return;
+        }
+
         quantity -= 1;
         display(quantity);
+    }
+
+    public void order(View view) {
+        int price = quantity * pizzaPrice;
+
+        String priceString = String.valueOf(NumberFormat.getCurrencyInstance().format(price));
+        String priceMessage = "Total value: " + priceString + " Thank you.";
+
+        displayMessage(priceMessage);
+    }
+
+    private void displayMessage(String message) {
+        priceTextView.setText(message);
     }
 }
